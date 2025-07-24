64d4444dd9fad3ddb60ab70b2259d1588172bc49
rahmaAlmamari <alr816125@gmail.com>
2025-07-24 14:54:24 +0400

    Adding IWithdrawable interface

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿namespace ExampleInterface1
 {
+    //1. IAccount interface ...
     public interface IAccount
     {
         string AccountNumber { get; }
         decimal Balance { get; }
 
         void Deposit(decimal amount);
+    }
+    //2. IWithdrawable interface ...
+    public interface IWithdrawable
+    {
     }
     internal class Program
     {
